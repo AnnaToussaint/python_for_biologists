241183ad26204c8f479e495558215885050cd914
Anna Toussaint <annatouss@gmail.com>
2022-08-08 16:56:54 -0400

    Added assigning variables.

--- pythonforbiologists.py
+++ pythonforbiologists.py
@@ -1,14 +1,29 @@
 ##Python for Biologists by Dr. Martin Jones
-
+ 
 
 #Applying the print() function 
 
-        #print("Hello world!")
-        #print('Hello world!')
+#print("Hello world!")
+#print('Hello world!')
         
-        #escaping characters 
-           #print("The name is \\\"test\".")
-
+#escaping characters 
+#print("The name is \\\"test\".")
 
 #printing on a new line
-print("Hello \n world!")
+#print("Hello \nworld!")
+
+#storing strings in variables
+
+my_dna = "ATGCGTA"
+print(my_dna)
+
+
+
+
+
+
+
+
+
+
+
